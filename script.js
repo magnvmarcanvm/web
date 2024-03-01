db17e836f3f655bce6ab206bc620b5b67d104afa
magnvmarcanvm <159582226+magnvmarcanvm@users.noreply.github.com>
2024-03-01 09:15:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -19,17 +19,29 @@ function checkPassword1() {
     if (passwordInput1 === correctPassword1) {
         window.location.href = "spqr.html"; // Cambia a la página que desees
     } else {
-        alert("X");
+        alert("Incorrect");
     }
 }
 
-const correctPassword2 = ""; // Cambia esto a tu clave secreta
+const correctPassword2 = "SOL"; // Cambia esto a tu clave secreta
 
 function checkPassword2() {
     const passwordInput2 = document.getElementById("password2").value;
     
     if (passwordInput2 === correctPassword2) {
-        window.location.href = "spqr.html"; // Cambia a la página que desees
+        window.location.href = "main.html"; // Cambia a la página que desees
+    } else {
+        alert("Incorrect");
+    }
+}
+
+const correctPasswordColum = "DXIVI"; // Cambia esto a tu clave secreta
+
+function checkPasswordColum() {
+    const passwordInputColum = document.getElementById("passwordColum").value;
+    
+    if (passwordInputColum === correctPasswordColum) {
+        alert("NUMERUS TURUS EST   V");
     } else {
         alert("Incorrect");
     }
